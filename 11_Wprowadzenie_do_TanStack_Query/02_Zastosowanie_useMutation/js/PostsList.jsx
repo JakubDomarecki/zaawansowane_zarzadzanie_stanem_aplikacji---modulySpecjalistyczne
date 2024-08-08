ff88eb65ec6f8d5d05be430a7811eaf825257cb9
JakubDomarecki <kuba86952@wp.pl>
2024-08-08 14:45:04 +0200

    resolved ex2 tanStack_Query

--- 11_Wprowadzenie_do_TanStack_Query/02_Zastosowanie_useMutation/js/PostsList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/02_Zastosowanie_useMutation/js/PostsList.jsx
@@ -1,13 +1,118 @@
+import { useQueryClient , useMutation, useQuery} from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+
+const fetchPosts = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/posts')
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+  catch (err) {
+    throw new Error ('fetch error' + err.message);
+  }
+}
+
+const addPost = async (newPost) => {
+  try {
+    const response = await fetch('http://localhost:3001/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newPost),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.json();
+  } catch (err) {
+    throw new Error('Add post error: ' + err.message);
+  }
+}
+
+const deletePost = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:3001/posts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.json();
+  } catch (err) {
+    throw new Error('Delete error: ' + err.message);
+  }
+}
+
 export const PostsList = () => {
-  return (
+  const {data, error, isPending, isError} = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  })
+
+  const {register, handleSubmit, reset} = useForm()
+
+
+  const queryClient = useQueryClient();
+
+  const deletePostMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts']);
+    },
+  })
+
+  const AddMutation = useMutation({
+    mutationFn: addPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts'])
+      reset()
+    },
+  })
+
+  const onSubmit = (formData) => {
+    AddMutation.mutate(formData);  
+  }
+
+
+  if (isPending) {
+    return <div>loading...</div>
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>
+  }
+
+  if (data) {
+    return (
     <div>
-      <form>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Body" />
-        <button>Add Post</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Title" {...register('title', {required: true})} />
+        <textarea placeholder="Body"  {...register('body', {required: true})}/>
+        <button type="submit">Add Post</button>
       </form>
-      {/* Przycisk do usuwania posta; użyj odpowiednio w kontekście listy postów */}
-      {/* <button>Delete Post</button> */}
+
+      <div>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.body}</p>
+              <button onClick={() => deletePostMutation.mutate(item.id)}>DELETE</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+}
 };
