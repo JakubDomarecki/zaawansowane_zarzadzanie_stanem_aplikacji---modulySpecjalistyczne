3dd5c0e44501049b046dc3d3f5e34ec7c5b266e6
JakubDomarecki <kuba86952@wp.pl>
2024-08-08 13:34:43 +0200

    resolved ex1 TanStack_Query

--- 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
@@ -1,3 +1,65 @@
+import { useQuery } from "@tanstack/react-query";
+
+
+const fetchPosts = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(resp => {
+    if (!resp.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return resp.json()
+  })
+  .catch(err => {
+    throw new Error ('fetch error' + err.message);
+  })
+}
+
+// const fetchPosts2 = async () => {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//      if (!response.ok) {
+//        throw new Error('Network response was not ok');
+//      }
+//       return response.json()
+//      }
+//   catch (err) {
+//       throw new Error ('fetch error' + err.message);
+//     }
+// }
+
+
 export const PostsList = () => {
-  return null;
+
+  const {isPendind, isError, error, data} = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  })
+
+
+    if (isPendind) {
+      return <div>loading...</div>
+    }
+
+    if (isError) {
+      return <div>{error.message}</div>
+    }
+
+    if (data) {
+      return (
+        <div>
+          <h1>posts</h1>
+          <ul>
+            {data.map(post => (
+              <li key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+
+
+
 };
